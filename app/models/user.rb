c69775c48995f9916b83005bc2e5b02527938928
gannagoodkevich <gannagoodkevich@gmail.com>
2019-12-03 13:33:02 +0300

    Create Post model, add has_one, has_many relation between user and post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   belongs_to :organization
+  has_many :posts
   validates :active, presence: true
   validates :name, presence: true, length: { maximum: 50 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_085525) do
+ActiveRecord::Schema.define(version: 2019_12_03_101201) do
 
   create_table "organizations", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.string "status"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
