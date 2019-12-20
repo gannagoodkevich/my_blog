5710afddfbf42ed29062faf596a05e96cbe43387
gannagoodkevich <gannagoodkevich@gmail.com>
2019-12-20 13:06:38 +0300

    Make attr_permit and dig parameters, create user from organization object

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   def index
-    #@posts = User.includes(:posts).where(organization_id: params[:organization_id])
-    @users = User.where(organization_id: params[:organization_id])
-    @users = @users.page params[:page]
+    @users = User.where(organization_id: params.dig(:organization_id))
+    @users = @users.page(params.dig(:page))
   end
 
   def new
@@ -11,29 +10,33 @@ class UsersController < ApplicationController
 
 
   def edit
-    @user = User.find(params[:id])
+    @user = User.find(params.dig(:id))
   end
 
   def create
-    @organization = Organization.find(params[:organization_id])
-    @user = User.new(name: params[:user][:name], active: params[:user][:status], organization_id: params[:organization_id])
-    @user.save!
-    #User.find_by(name: params[:post][:name]).posts << @post
-    #User.find_by(name: params[:post][:name]).save!
+    @organization = Organization.find(params.dig(:organization_id))
+    @organization.users.create!(attr_user)
     redirect_to organization_users_path(@organization)
   end
 
   def update
-    @organization = Organization.find(params[:organization_id])
-    @user = User.find(params[:id])
-    @user.update!(active: params[:button][:active])
-    #User.find_by(name: params[:post][:name]).posts << @post
-    #User.find_by(name: params[:post][:name]).save!
+    @organization = Organization.find(params.dig(:organization_id))
+    User.find(params.dig(:id)).update!(attr_button)
     redirect_to organization_user_path
   end
 
   def show
-    @user = User.find(params[:id])
-    @posts = @user.posts.page params[:page]
+    @user = User.find(params.dig(:id))
+    @posts = @user.posts.page(params.dig(:page))
+  end
+
+  private
+
+  def attr_user
+    params.require(:user).permit(:name, :active)
+  end
+
+  def attr_button
+    params.require(:button).permit(:active)
   end
 end
