0ce2b4c483d2369e9af025a3f15bbcfe9ce662bd
gannagoodkevich <gannagoodkevich@gmail.com>
2020-01-08 18:43:03 +0300

    Make resque ScriptError and raise StandartError to make retry

--- app/jobs/error_job.rb
+++ app/jobs/error_job.rb
@@ -1,12 +1,16 @@
 class ErrorJob < ApplicationJob
-  MyError = Class.new(ScriptError)
+  MyError = Class.new(StandardError)
   queue_as :error_retry
 
-  retry_on(LoadError, wait: 1.second, attempts: 6) do
+  retry_on(StandardError, wait: 1.hour, attempts: 6) do
     puts "Error"
   end
 
   def perform(*args)
-    raise LoadError
+    begin
+      require 'fake_path'
+    rescue ScriptError => e
+      raise MyError
+    end
   end
 end
