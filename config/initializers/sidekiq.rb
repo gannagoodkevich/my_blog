f95b0c64564f72b67cccb0ff1dc40466e1fca0bc
gannagoodkevich <gannagoodkevich@gmail.com>
2020-01-10 10:57:12 +0300

    Make error job perform_later

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,5 +2,5 @@ schedule_file = "config/schedule.yml"
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash(YAML.load_file(schedule_file))
   ScheduleWorker.perform_async
-  ErrorJob.perform_now
+  ErrorJob.perform_later
 end
